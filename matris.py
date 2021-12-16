e108da04b0590bc1abebf49721245d8e5f43a39c
Pieter Zeilstra <kopop8@live.nl>
2021-12-16 14:08:05 +0100

    Added get_holes() plus ui for it

--- matris.py
+++ matris.py
@@ -69,6 +69,8 @@ class Matris(object):
 
         self.combo = 1 # Combo will increase when you clear lines with several tetrominos in a row
         
+        self.holes = 0
+
         self.paused = False
 
         self.highscore = load_score()
@@ -351,11 +353,38 @@ class Matris(object):
 
         self.set_tetrominoes()
 
+        #Here calculate states?
+        self.holes = self.get_holes()
+
         if not self.blend():
             self.gameover_sound.play()
             self.gameover()
             
         self.needs_redraw = True
+
+    def get_holes(self):
+        holes = 0
+        for item in self.matrix.items():
+            block = item[1]
+            idx = item[0]
+            # look items around
+            item_around = 0
+            if block is None:
+                # above
+                if (idx[0]-1,idx[1]) not in self.matrix.keys() or self.matrix[idx[0]-1,idx[1]]:
+                    item_around += 1
+                # below
+                if (idx[0]+1,idx[1]) not in self.matrix.keys() or self.matrix[idx[0]+1,idx[1]]:
+                    item_around += 1
+                # left
+                if (idx[0],idx[1]-1) not in self.matrix.keys() or self.matrix[idx[0],idx[1]-1]:
+                    item_around += 1
+                # right
+                if (idx[0],idx[1]+1) not in self.matrix.keys() or self.matrix[idx[0],idx[1]+1]:
+                    item_around += 1
+                if item_around >=3 and (idx[0]-1,idx[1]) in self.matrix.keys():
+                    holes+=1
+        return holes
 
     def remove_lines(self):
         """
@@ -485,11 +514,14 @@ class Game(object):
         levelsurf = renderpair("Level", self.matris.level)
         linessurf = renderpair("Lines", self.matris.lines)
         combosurf = renderpair("Combo", "x{}".format(self.matris.combo))
+        holessurf = renderpair("Holes", self.matris.holes)
 
         height = 20 + (levelsurf.get_rect().height + 
                        scoresurf.get_rect().height +
                        linessurf.get_rect().height + 
-                       combosurf.get_rect().height )
+                       combosurf.get_rect().height +
+                       holessurf.get_rect().height
+                       )
         
         #Colours side panel
         area = Surface((width, height))
@@ -501,7 +533,7 @@ class Game(object):
         area.blit(scoresurf, (0, levelsurf.get_rect().height))
         area.blit(linessurf, (0, levelsurf.get_rect().height + scoresurf.get_rect().height))
         area.blit(combosurf, (0, levelsurf.get_rect().height + scoresurf.get_rect().height + linessurf.get_rect().height))
-
+        area.blit(holessurf, (0, levelsurf.get_rect().height + scoresurf.get_rect().height + linessurf.get_rect().height +holessurf.get_rect().height))
         screen.blit(area, area.get_rect(bottom=HEIGHT-MATRIS_OFFSET, centerx=TRICKY_CENTERX))
 
 

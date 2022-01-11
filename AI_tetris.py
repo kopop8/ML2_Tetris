028cc52f4ecb41aa6cb2a637fa3b7dc7f0ca46cc
Pieter Zeilstra <kopop8@live.nl>
2022-01-11 12:21:50 +0100

    Print score

--- AI_tetris.py
+++ AI_tetris.py
@@ -4,4 +4,4 @@ import numpy as np
 matris.start_game()
 best_weights = np.array([-1.41421812,  3.31925469,  1.75633084, -0.68687555, -2.51168949,
    -3.67272167, -2.48923022, -2.98793366, -3.92940647])
-matris.start_round_GA(best_weights, [0,0,0,0])
+print('Score', matris.start_round_GA(best_weights, [0,0,0,0,0,0]))

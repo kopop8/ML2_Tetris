c227af2f55d7b5f6a4e72c5ca5418c05b329e027
Pieter Zeilstra <kopop8@live.nl>
2022-01-21 14:07:05 +0100

    Fixed saving file

--- genetic_agent.py
+++ genetic_agent.py
@@ -2,7 +2,10 @@ import numpy as np
 import ga
 import csv
 import matplotlib.pyplot as plt
-
+import pandas as pd
+from numpy import savetxt
+from numpy import loadtxt
+import pickle
 
 def train_agent(num_generations, pop_size, new_population, num_parents_mating, num_mutations,mutate_percentage, max_lines_cleared=False):
     print("Beginning training")
@@ -24,40 +27,14 @@ def train_agent(num_generations, pop_size, new_population, num_parents_mating, n
         best_match_idx = np.where(fitness == np.max(fitness))
     # print(new_population)
         print("Best solution : ", new_population[best_match_idx, :])
+        #data.append([generation + 1, np.max(fitness), new_population[best_match_idx, :]])
+        data.append(new_population)
+        # df = pd.DataFrame(data, columns=["generation", "fitness", "population"])
+        # df.to_csv('myfile.csv')
+        output = open('data.pkl', 'wb')
+        pickle.dump(data, output)
+        output.close()
 
-
-
-        try:
-            data = []
-            with open('scores.csv', 'r') as f:
-                csv_reader = csv.reader(f)
-                for line in csv_reader:
-                    if line != '':
-                        data.append(line)
-            #data.append([generation + 1, np.max(fitness), new_population[best_match_idx, :]])
-            data.append([generation + 1, np.max(fitness), new_population])
-            list2 = [x for x in data if x != []]
-            with open('scores.csv', 'w', encoding='UTF8') as f:
-                writer = csv.writer(f)
-
-                for x in list2:
-                    # write the data
-                    writer.writerow(x)
-        except:
-            print("File does not exist, so create file with ")
-            header = ['generation', 'score', 'weights']
-            #data.append([generation + 1, np.max(fitness), new_population[best_match_idx, :]])
-            data.append([generation + 1, np.max(fitness), new_population])
-            list2 = [x for x in data if x != []]
-
-            with open('scores.csv', 'w', encoding='UTF8') as f:
-                writer = csv.writer(f)
-
-                writer.writerow(header)
-
-                for x in list2:
-                    # write the data
-                    writer.writerow(x)
         
         # Selecting the best parents in the population for mating.
         parents = ga.select_mating_pool(new_population, fitness, 
@@ -88,35 +65,27 @@ def train_agent(num_generations, pop_size, new_population, num_parents_mating, n
 
 #Creating the initial population.
 num_weights = 99
-max_lines_cleared = 20
-sol_per_pop = 5
+max_lines_cleared = 100
+sol_per_pop = 100
 num_generations = 5
-num_parents_mating = int(sol_per_pop*0.2)
-num_mutations = int(sol_per_pop*0.2)
+num_parents_mating = int(sol_per_pop*0.1)
+num_mutations = int(sol_per_pop*0.1)
 mutate_percentage = 0.5
 
 # Defining the population size.
 pop_size = (sol_per_pop,num_weights) # The population will have sol_per_pop chromosome where each chromosome has num_weights genes.
 
 def GET_best_weights():
-    with open('scores.csv', "r") as f1:
-        data = []
-        csv_reader = csv.reader(f1)
-        for line in csv_reader:
-            data.append(line)
-        array = data[-2][2]
-        array = array.replace('  ', ',')
-        array = array.replace(' -', ',-')
-        return array
-
-#print(np.random.uniform(low=-4.0, high=4.0, size=pop_size))
-
+    # data = pd.read_csv('myfile.csv').to_numpy()
+    # return np.fromstring(data[-1][3])
+    pkl_file = open('data.pkl', 'rb')
+    data1 = pickle.load(pkl_file)[-1]
+    pkl_file.close()
+    return data1
+    
 try:
-    data = GET_best_weights()
-    del data[0]
-    del data[-1]
-    new_population = [data]
-except:
+    new_population = GET_best_weights()
+except:                                                  
     new_population = np.random.uniform(low=-4.0, high=4.0, size=pop_size)
-
+print(new_population)
 train_agent(num_generations, pop_size, new_population, num_parents_mating, num_mutations, mutate_percentage, max_lines_cleared)
